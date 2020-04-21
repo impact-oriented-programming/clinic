588d54d056539356468bb4788cd0d1385a29d931
Maya Mann <maya.mann2@gmail.com>
2020-04-21 19:39:37 +0300

    update installed apps in clinic project with session app

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'general_models.apps.GeneralModelsConfig',
+    'session_models.apps.SessionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
