04b64bc8d0de3a4fd815e19fec9324906aa367db
Maya Mann <maya.mann2@gmail.com>
2020-04-26 20:40:42 +0300

    update doctor class to contain a doctor user

--- clinic/general_models/models.py
+++ clinic/general_models/models.py
@@ -2,20 +2,17 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
 
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     specialty = models.CharField(max_length=30)
-    email = models.EmailField()
     license_number = models.CharField(max_length=30, blank=True)
     def __str__(self):
-        return (self.first_name + " " + self.last_name)
-    
+        return self.user.username
+
 @receiver(post_save, sender=User)
 def create_doctor_profile(sender, instance, created, **kwargs):
     if created:
@@ -42,7 +39,7 @@ class Patient(models.Model):
         return (self.first_name + " " + self.last_name)
 
 class Appointment(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    #doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
