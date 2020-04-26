2690df19bec648b76a6b98fe5dc00ac3276518a2
Maya Mann <maya.mann2@gmail.com>
2020-04-26 20:11:04 +0300

    try to add doctor user into doctor class

--- clinic/clinic/urls.py
+++ clinic/clinic/urls.py
@@ -1,3 +1,4 @@
+
 """clinic URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
--- clinic/general_models/models.py
+++ clinic/general_models/models.py
@@ -1,8 +1,13 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     specialty = models.CharField(max_length=30)
@@ -10,6 +15,16 @@ class Doctor(models.Model):
     license_number = models.CharField(max_length=30, blank=True)
     def __str__(self):
         return (self.first_name + " " + self.last_name)
+    
+@receiver(post_save, sender=User)
+def create_doctor_profile(sender, instance, created, **kwargs):
+    if created:
+        Doctor.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_doctor_profile(sender, instance, **kwargs):
+    instance.doctor.save()
+    
 
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
