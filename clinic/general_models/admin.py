704f5909f6ddaa493903ecce571dd791428b9d4b
Maya Mann <maya.mann2@gmail.com>
2020-04-26 17:30:30 +0300

    register doctor, patient and appointment models with admin

--- clinic/general_models/admin.py
+++ clinic/general_models/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Doctor, Patient, Appointment
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
