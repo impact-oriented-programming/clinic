b6ac4a7c5907a51d226d4481af643c1e2af22646
LiorCGit <liorc5@mail.tau.ac.il>
2020-05-03 21:17:51 +0300

    updated create new patient style, redirects to clinic on creation. some links are useless - will be removed later

--- clinic/create_new_patient/views.py
+++ clinic/create_new_patient/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import CreatePatientForm
+from django.contrib import messages
 
 
 def index(request):
@@ -7,10 +8,13 @@ def index(request):
         form = CreatePatientForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            messages.success(request, f'Account created for {first_name} {last_name}!')
+            return redirect('clinic_calendar:calendar')
     else:
         form = CreatePatientForm()
-    return render(request, 'create_new_patient/create_new_patient.html', {'form': form})
+    return render(request, 'create_new_patient/create_new_patient.html', {'form': form, 'title': 'Create New Patient'})
 
 
 
