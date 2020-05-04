9efa8f1e84e27b1b972cb7dfdce24827417ce6a7
EdenKimelman <edenk1237@gmail.com>
2020-05-04 16:42:04 +0300

    paitent interface temp update

--- clinic/patient_interface/views.py
+++ clinic/patient_interface/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+import general_models.models as gm
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the PATIENT INTERFACE index.")
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the CLINIC CALENDAR index.")
+
+class index(View):
+    
+    def get(self, request, *args, **kwargs):
+        
+        all_patients = gm.Patient.objects.all()
+        testing_patient = all_patients[0]
+        all_patient_appointments = gm.Appointment.objects.all()
+        all_patient_appointments = all_patient_appointments.filter(patient = testing_patient).order_by("date")
+        
+        context = {
+            'patient': testing_patient, 
+            'last_visits': all_patient_appointments
+            }
+        return render(request, 'patient_interface/patient_interface_home.html', context)
+        
+
