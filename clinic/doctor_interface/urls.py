9675e7f5c7a88b53296e48a6b8fa03be51f9ecd3
Maya Mann <maya.mann2@gmail.com>
2020-05-05 17:37:27 +0300

    update links inside html files

--- clinic/doctor_interface/urls.py
+++ clinic/doctor_interface/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 from . import views
 
+app_name = 'doctor_interface'
 urlpatterns = [
     path('', views.index.as_view() , name='homepage'),
     path('patient_interface/', views.index_patient.as_view() , name='patient_interface'),
