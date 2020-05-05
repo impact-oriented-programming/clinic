646fd2ebcb5e454564f1f0041a563dfd62ab1013
Maya Mann <maya.mann2@gmail.com>
2020-05-05 11:50:53 +0300

    delete event model that comes from calendar tuturial

--- clinic/clinic_calendar/models.py
+++ clinic/clinic_calendar/models.py
@@ -9,9 +9,3 @@ from django.urls import reverse
 # calendar view of all the scheduled appointments
 # mark a person that showed up to his appointemnt
 
-class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    
