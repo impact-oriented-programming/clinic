09bd1a8241520c6d4c0bbcebd53882e1af1969e6
EdenKimelman <edenk1237@gmail.com>
2020-05-25 15:17:19 +0300

    calculating age for patient

--- clinic/doctor_interface/views.py
+++ clinic/doctor_interface/views.py
@@ -25,11 +25,13 @@ def index_patient(request, clinic_id):
         return render(request, 'doctor_interface/error_patient_not_found.html')
        
     patient_filter = patient_filter[0]  # was list of length 1. we want the patient itselfs
-    #age = datetime.datetime.now().year
+    today = datetime.date.today()
+    born = patient_filter.date_of_birth
+    patient_age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
     last_visits = Session.objects.all()
     max_session = min(5, len(last_visits))
     last_visits = last_visits.filter(patient=patient_filter)[:max_session]
-    context = {'patient': patient_filter, 'last_visits': last_visits} #, "age": str(age)}
+    context = {'patient': patient_filter, 'last_visits': last_visits, 'age_value': str(patient_age)}
     return render(request, 'doctor_interface/patient_interface_home.html', context)
 
 
