83fe6eee47d581af7e4d49c0c95be460b1c8732b
Maya Mann <maya.mann2@gmail.com>
2020-06-01 09:36:30 +0300

    change admin appointment view to show details

--- clinic/general_models/admin.py
+++ clinic/general_models/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Doctor, Patient, Appointment
-# Register your models here.
+
+
+class Custoimize_Appointments(admin.ModelAdmin):
+    list_display = ('id', 'date', 'start_time', 'doctor', 'patient')
+
+
 admin.site.register(Doctor)
 admin.site.register(Patient)
-admin.site.register(Appointment)
+admin.site.register(Appointment, Custoimize_Appointments)
+
