2f49d347f478d84544ea155ca02a945e8e6234b7
EdenKimelman <edenk1237@gmail.com>
2020-06-17 17:07:06 +0300

    adding preparation for Onni scripts

--- clinic/doctor_interface/management/commands/add_medical_db.py
+++ clinic/doctor_interface/management/commands/add_medical_db.py
@@ -6,18 +6,37 @@ class Command(BaseCommand):
     help = 'Loads medical datebase including diagnosis, medication and blood tests.'
 
     def handle(self, *args, **options):
-            file = open('./doctor_interface/medical_db/blood_tests.txt', 'r')
+            
+            self.add_blood_tests()
+            self.add_medications()
+            self.add_diagnosis()
 
-            for line in file.readlines():
-                array = line.split('|')
-                test_code = int(array[0])
-                test_desc = array[1]
-                test_price = int(array[2])
-                test_group = array[3]
-                test = BloodTest(blood_test = test_desc, blood_test_code = test_code, price = test_price, group = test_group)
-                test.save()
+            self.stdout.write(self.style.SUCCESS('Successfully uploaded medical datebase!'))
+        
+        
+    #Helper Functions    
+    def add_blood_tests(self):
+        
+        file = open('./doctor_interface/medical_db/blood_tests.txt', 'r')
 
-            file.close()
+        for line in file.readlines():
+            array = line.split('|')
+            test_code = int(array[0])
+            test_desc = array[1]
+            test_price = int(array[2])
+            test_group = array[3]
+            test = BloodTest(blood_test = test_desc, blood_test_code = test_code, price = test_price, group = test_group)
+            test.save()
 
-            self.stdout.write(self.style.SUCCESS('Successfully upload medical datebase!'))
+        file.close()
+            
+    def add_medications(self):
+        
+        #TODO (Onni)
+        
+        return
+    def add_diagnosis(self):
+        
+        #TODO (Onni)
     
+        return
