6df05dedf4b50dfe8ecb501d320f3ba553e71e08
Onni Amir <onniamir@gmail.com>
2020-06-18 11:49:36 +0300

    add scripts and files for add_med_db

--- clinic/doctor_interface/management/commands/add_medical_db.py
+++ clinic/doctor_interface/management/commands/add_medical_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from doctor_interface.models import BloodTest
+from doctor_interface.models import BloodTest, Diagnosis, Medication
+import pandas as pd
 import os
 
 class Command(BaseCommand):
@@ -43,14 +44,70 @@ class Command(BaseCommand):
             test.save()
 
         file.close()
-            
+
     def add_medications(self):
-        
-        #TODO (Onni)
-        
-        return
+        self.add_prescription_medications()
+        self.add_no_prescription_medications()
+
+    def add_prescription_medications(self):
+
+        presc_med = pd.read_excel('./doctor_interface/medical_db/PrescriptionsMed.xlsx', header=None, skiprows=2).T
+
+        med_presc_dic = presc_med.to_dict("list")
+        ctr = 0
+        for med in med_presc_dic:
+            code = med_presc_dic[med][0]  # A col
+            name = med_presc_dic[med][1]  # B col
+            yrpa_code = med_presc_dic[med][7]  # H col, can be null
+            if yrpa_code != yrpa_code:
+                yrpa_code = None
+            else:
+                yrpa_code = int(med_presc_dic[med][7])
+            pharmasoft_code = med_presc_dic[med][8]  # I col, can be null
+            if pharmasoft_code != pharmasoft_code:
+                pharmasoft_code = None
+            else:
+                pharmasoft_code = int(med_presc_dic[med][8])
+            medication = Medication(medication=name, medication_code=code, medication_details=None,
+                                    medication_yrpa_code=yrpa_code, medication_pharmasoft_code=pharmasoft_code,
+                                    prescription_required=True)
+            medication.save()
+            ctr += 1
+
+    def add_no_prescription_medications(self):
+
+        no_presc_med = pd.read_excel('./doctor_interface/medical_db/NoPrescriptionsMed.xlsx', header=None, skiprows=2).T
+
+        med_no_presc_dic = no_presc_med.to_dict("list")
+        ctr = 0
+        for med in med_no_presc_dic:
+            code = med_no_presc_dic[med][0]  # A col
+            name = med_no_presc_dic[med][1]  # B col
+            details = med_no_presc_dic[med][2]  # C col
+            yrpa_code = med_no_presc_dic[med][6]  # G col, can be null
+            if yrpa_code != yrpa_code:
+                yrpa_code = None
+            else:
+                yrpa_code = int(med_no_presc_dic[med][6])
+            pharmasoft_code = med_no_presc_dic[med][7]  # H col, can be null
+            if pharmasoft_code != pharmasoft_code:
+                pharmasoft_code = None
+            else:
+                pharmasoft_code = int(med_no_presc_dic[med][7])
+            medication = Medication(medication=name, medication_code=code, medication_details=details,
+                                    medication_yrpa_code=yrpa_code, medication_pharmasoft_code=pharmasoft_code,
+                                    prescription_required=False)
+            medication.save()
+            ctr += 1
+
     def add_diagnosis(self):
-        
-        #TODO (Onni)
+
+        diag_file = open('./doctor_interface/medical_db/icd10cm_codes_2019.txt', 'r')
+
+        for line in diag_file.readlines():
+            diag_code = line.split()[0]
+            diag_desc = ' '.join(line.split()[1:])
+            diag = Diagnosis(diagnosis=diag_desc, diagnosis_code=diag_code)
+            diag.save()
     
-        return
+        diag_file.close()
